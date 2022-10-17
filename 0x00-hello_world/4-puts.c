da5ec8717ee544423b6c124c14797d8f7a795ec9
ferddymalemo08 <fmalemo@jkuat.ac.ke>
2022-10-17 02:09:32 -0700

    C program printing a string

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+/**
+ * main - Entry Point
+ *
+ * Return: Always 0 Success
+ */
+
+int main(void)
+{
+	char mystr = "\"Programming is like building a puzzle"
+
+	puts(mystr);
+
+	return (0);
+}
