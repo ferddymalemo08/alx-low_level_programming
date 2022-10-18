f7632b2ba56069019f1cb0d54479ab94572ad789
ferddymalemo08 <fmalemo@jkuat.ac.ke>
2022-10-18 11:38:21 -0700

    0-putchar.c

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,22 @@
+#include "main.h"
+
+/**
+ * main - prints _putchar and a new line
+ *
+ * Return: Always 0 success
+ */
+
+int main(void)
+{
+	putchar('-');
+	putchar('p');
+	putchat('u');
+	putchar('t');
+	putchar('c');
+	putchar('h');
+	putchar('a');
+	putchar('r');
+	putchar('\n');
+
+	return (0);
+}
---
+++ 0x02-functions_nested_loops/README.md
@@ -0,0 +1,2 @@
+##0x02. C - Functions, nested loops
+1.Task-0: Write a program that prints _putchar, followed by a new line
