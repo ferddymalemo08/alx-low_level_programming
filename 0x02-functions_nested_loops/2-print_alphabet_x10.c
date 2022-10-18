bc09a3a21d1dd1740efe5065844eee9fe739e477
ferddymalemo08 <fmalemo@jkuat.ac.ke>
2022-10-18 13:21:25 -0700

    Printing alphabets in lowercase x10

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+/**
+ * print_alphabet_x10 - writes the alphabet on the stdout x10
+ */
+
+void print_alphabet_x10(void)
+{
+	int xten;
+	char la;
+
+	for (xten = 0; xten <= 9; xten++)
+	{
+		for (la = 'a'; la <= la; la++)
+			_putchar(la);
+		_putchar('\n');
+	}
+}
+
