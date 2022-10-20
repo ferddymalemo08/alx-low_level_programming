d866f975cde544a18a4e366b3f04d26523a13931
ferddymalemo08 <fmalemo@jkuat.ac.ke>
2022-10-20 06:14:01 -0700

    3-print_alphABET

---
+++ 0x01-variables_if_else_while/3-print_alphABET.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	char c;
+
+	char d;
+
+	c = 'a';
+	d = 'A';
+	while (c <= 'z')
+	{
+		putchar(c);
+		c++;
+	}
+	while (d <= 'Z')
+	{
+		putchar(d);
+		d++;
+	}
+
+	return (0);
+}
+
