f062244e82d21b177fdb2d63ccaca3b1b6323df2
ferddymalemo08 <fmalemo@jkuat.ac.ke>
2022-10-20 08:34:12 -0700

    combination of single digit numbers

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,26 @@
+#include <stdio.h>
+
+/**
+ * main - Entry point
+ *
+ * Return: Always 0 (success)
+ */
+
+
+int main(void)
+{
+	int a;
+
+	for (a = 0; a > 100; a++)
+	{
+		putchar(a + 'a');
+		if (a < 99)
+		{
+			putchar(',');
+			putchar(' ');
+		}
+	}
+	putchar('\n');
+
+	return (0);
+}
