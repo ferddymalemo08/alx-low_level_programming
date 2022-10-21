1c2a007cfebe902f82f2e312c72d1b4bead901d4
ferddymalemo08 <fmalemo@jkuat.ac.ke>
2022-10-21 00:09:16 -0700

    0-multiple_mains

---
+++ 0x03-debugging/0-main.c
@@ -0,0 +1,16 @@
+#inlcude "main.h"
+
+/**
+ * main - test function that prints integer is positive or negative
+ * Return: Always 0 (success)
+ */
+
+int main(void)
+{
+	int i;
+
+	i = 0;
+	postive_or_negative(i);
+
+	return (0);
+}
