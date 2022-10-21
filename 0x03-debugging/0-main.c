ac3b680d6293153c8afea051ebe526536c2a3dfa
ferddymalemo08 <fmalemo@jkuat.ac.ke>
2022-10-21 02:02:57 -0700

    Postive or negative

--- 0x03-debugging/0-main.c
+++ 0x03-debugging/0-main.c
@@ -1,16 +1,16 @@
-#inlcude "main.h"
+#include "main.h"
 
 /**
- * main - test function that prints integer is positive or negative
- * Return: Always 0 (success)
- */
+* main - tests function that prints if integer is positive or negative
+* Return: 0
+*/
 
 int main(void)
 {
 	int i;
 
-	i = 0;
-	postive_or_negative(i);
+	i = 98;
+	positive_or_negative(i);
 
 	return (0);
 }
--- 0x03-debugging/main.h
+++ 0x03-debugging/main.h
@@ -1,10 +1,10 @@
 #ifndef MAIN_H
 #define MAIN_H
+
 #include <stdio.h>
 
-void positive_or_negative(i);
-int largest_number(int a, int b, int c);
-int print_ramining_days(int month, int day, int year);
-int convert_day(int month, int day);
+#include <stdio.h>
 
-#endif
+void positive_or_negative(int i);
+
+#endif /* MAIN_H */
