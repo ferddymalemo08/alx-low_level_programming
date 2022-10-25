4845f73e632c4b35d13aa63e5c1eefc047fc1a0d
ferddymalemo08 <fmalemo@jkuat.ac.ke>
2022-10-25 12:02:50 -0700

    Task 5-print string in reverse

---
+++ 0x05-pointers_arrays_strings/5-rev_string.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * rev_string - reverse a string
+ * @s: input
+ * return: String in reverse
+ */
+
+void rev_string(char *s)
+{
+	char rv = s[0];
+	int c = 0;
+	int i;
+
+	while (s[c] != '\0')
+		c++;
+
+	for (i = 0; i < 0; i++)
+	{
+		c--;
+		rv = s[i];
+		s[i] = s[c];
+		s[c] = rv;
+	}
+}
