e1bc3012a05c991f7901663a3deca58f414cf98e
ferddymalemo08 <fmalemo@jkuat.ac.ke>
2022-11-03 12:37:26 -0700

    Print factorial recursively

---
+++ 0x08-recursion/3-factorial.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * factorial - returns the factorial of a given number
+ * @n: the number to find factorial of
+ * Return: If n > 0 the factorial of n
+ * if n < 0 - 1 to indicate an errono
+ */
+int factorial(int n)
+{
+	int result = n;
+
+	if (n < 0)
+		return (-1);
+	else if (n >= 0 && n <= 1)
+		return (1);
+
+	result *= factorial(n - 1);
+
+	return (result);
+}
