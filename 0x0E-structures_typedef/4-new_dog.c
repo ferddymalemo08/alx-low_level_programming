4efe127bf75209632eadadfea0e3b64ae7d23f23
ferddymalemo08 <fmalemo@jkuat.ac.ke>
2022-11-15 14:33:06 -0800

    new dog

--- 0x0E-structures_typedef/4-new_dog.c
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -4,9 +4,9 @@
 /**
  * _strlen - returns the length of a string
  * @s: string to evaluate
- *
  * Return: the length of the string
  */
+
 int _strlen(char *s)
 {
 	int i;
@@ -15,9 +15,11 @@ int _strlen(char *s)
 	while (s[i] != '\0')
 	{
 		i++;
+
 	}
 
 	return (i);
+
 }
 
 /**
@@ -40,42 +42,39 @@ char *_strcpy(char *dest, char *src)
 	}
 
 	for (i = 0; i < len; i++)
-
 	{
 		dest[i] = src[i];
 	}
 	dest[i] = '\0';
 	return (dest);
-}
 
+}
 /**
+ *
  * new_dog - creates a new dog
  * @name: name of the dog
  * @age: age of the dog
  * @owner: owner of the dog
  * Return: pointer to the new dog (Success), NULL otherwise
  */
-
 dog_t *new_dog(char *name, float age, char *owner)
 {
 	dog_t *dog;
 	int len1, len2;
-
+	
 	len1 = _strlen(name);
 	len2 = _strlen(owner);
 	dog = malloc(sizeof(dog_t));
-
 	if (dog == NULL)
 		return (NULL);
-
 	dog->name = malloc(sizeof(char) * (len1 + 1));
 	if (dog->name == NULL)
 	{
 		free(dog);
 		return (NULL);
 	}
-	dog->owner = malloc(sizeof(char) * (len2 + 1));
 
+	dog->owner = malloc(sizeof(char) * (len2 + 1));
 	if (dog->owner == NULL)
 	{
 		free(dog);
